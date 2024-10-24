e97f5637ea778f45bdc15ac91f75b5844c02b582
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2024-10-24 20:11:53 +0300

    Update MainClass.cs

--- IseseisevToo_ValeriaAllikTARpv23/MainClass.cs
+++ IseseisevToo_ValeriaAllikTARpv23/MainClass.cs
@@ -15,11 +15,18 @@ namespace IseseisevToo_ValeriaAllikTARpv23
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.Write("Ülesanne 1");
+            Console.WriteLine();
             Ulesanne1.Ulesanne_1();
+            Console.WriteLine();
+            Console.Write("Ülesanne 2");
+            Console.WriteLine();
             Ulesanne2.Ulesanne_2();
+            Console.WriteLine();
+            Console.Write("Ülesanne 3");
+            Console.WriteLine();
             Ulesanne3.Ulesanne_3();
         }
     }
 }
-
-
