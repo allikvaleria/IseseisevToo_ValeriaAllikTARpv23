a7d919ba8f594befb8f7ad0b722882ccc67075fb
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2024-10-24 20:13:02 +0300

    Update Ulesanne3.cs

--- IseseisevToo_ValeriaAllikTARpv23/Ulesanne3.cs
+++ IseseisevToo_ValeriaAllikTARpv23/Ulesanne3.cs
@@ -16,18 +16,22 @@ namespace IseseisevToo_ValeriaAllikTARpv23
 
         public static void Ulesanne_3()
         {
+            // Созданием список студентов с именами и оценками
             List<Student> students = new List<Student>
             {
                 new Student { Name = "Valeria Allik", Grades = new List<int> { 5, 4, 4, 5 } },
-                new Student { Name = "Daria Halchenko", Grades = new List<int> { 4, 3, 4, 4 } },
-                new Student { Name = "Sasha Semjonova", Grades = new List<int> { 4, 4, 5, 5 } },
-                new Student { Name = "Maria Gorbunova", Grades = new List<int> { 5, 5, 5, 5 } }
+                new Student { Name = "Daria Halchenko", Grades = new List<int> { 4, 5, 4, 4 } },
+                new Student { Name = "Sasha Semjonova", Grades = new List<int> { 4, 4, 3, 5 } },
+                new Student { Name = "Maria Gorbunova", Grades = new List<int> { 4, 3, 3, 5 } }
             };
-
+            // Список для хранения имен студентов, которые могут получить стипендию
             List<string> stipendStudents = new List<string>();
+            // Проходим по каждому студенту в списке
             foreach (var student in students)
             {
+                // Проверяем, все ли оценки студента 4 или выше
                 bool allGradesAreGood = student.Grades.All(grade => grade >= 4);
+                // Если все оценки подходят условию, добавляем имя студента в список стипендиатов
                 if (allGradesAreGood)
                 {
                     stipendStudents.Add(student.Name);
@@ -42,4 +46,3 @@ namespace IseseisevToo_ValeriaAllikTARpv23
         }
     }
 }
-
